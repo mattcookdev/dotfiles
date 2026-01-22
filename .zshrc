366fcce5df2904817571f02c5f4b621bd8db1226
mattcookdev <22277786+mattcookdev@users.noreply.github.com>
2026-01-22 12:09:55 +0800

    .

--- .zshrc
+++ .zshrc
@@ -144,3 +144,7 @@ export NVM_DIR="$HOME/Library/Application Support/Herd/config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 [[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
+
+
+# Herd injected PHP 8.4 configuration.
+export HERD_PHP_84_INI_SCAN_DIR="/Users/matt/Library/Application Support/Herd/config/php/84/"
--- fresh.sh
+++ fresh.sh
@@ -28,14 +28,14 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
+# brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
 # Set default MySQL root password and auth type
 # mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install imagick redis
+# pecl install imagick redis
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer
